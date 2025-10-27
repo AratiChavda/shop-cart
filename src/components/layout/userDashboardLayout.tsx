9c32e41d62ba50a9512817542effa062d114e0bc
AratiChavda <aratichavda31@gmail.com>
2025-10-27 14:25:03 +0530

    feat: footer added

--- src/components/layout/userDashboardLayout.tsx
+++ src/components/layout/userDashboardLayout.tsx
@@ -2,20 +2,24 @@ import { Outlet } from "react-router-dom";
 import { SidebarInset, SidebarProvider } from "../ui/sidebar";
 import { SidebarNav } from "./sidebar";
 import { Header } from "./userHeader";
+import Footer from "./footer";
 
 const UserDashboardLayout = () => {
   return (
     <SidebarProvider>
-      <div className="flex min-h-screen w-full">
-        <SidebarNav />
-        <SidebarInset className="flex-1">
-          <Header/>
-          <div className="flex flex-1 flex-col">
-            <div className="flex-1 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 w-full">
-              <Outlet />
+      <div className="flex min-h-screen w-full flex-col">
+        <div className="flex flex-1">
+          <SidebarNav />
+          <SidebarInset className="flex-1">
+            <Header />
+            <div className="flex flex-1 flex-col">
+              <div className="flex-1 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 w-full">
+                <Outlet />
+              </div>
             </div>
-          </div>
-        </SidebarInset>
+          </SidebarInset>
+        </div>
+        <Footer />
       </div>
     </SidebarProvider>
   );
